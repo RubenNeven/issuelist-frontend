09741a69fa6a7a6850293dd51ae2e5f53152f7f7
RubenNeven <rubenneven@gmail.com>
2020-04-09 17:45:56 +0200

    Issuelist: login - frontend

---
+++ src/main/java/com/tms/issuelist/IssuelistApplication.java
@@ -0,0 +1,13 @@
+package com.tms.issuelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/issuelist/IssuelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.issuelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
